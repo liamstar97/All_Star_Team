bbfcf4702f1064175dea4c4153ef270b5cce32ab
liamstar97 <liamstar@gmail.com>
2020-11-12 19:29:46 -0700

    changes to sql script naming conventions and order

--- Setup_Schema.sql
+++ Setup_Schema.sql
@@ -6,51 +6,10 @@
 CREATE SCHEMA IF NOT EXISTS Allstar_Team;
 USE Allstar_Team;
 
-# Create table CHAMPIONSHIP_TEAM if it does not already exist
-CREATE TABLE IF NOT EXISTS CHAMPIONSHIP_TEAM
-(
-  Id                  INT UNIQUE    NOT NULL,
-  Team_name           VARCHAR(45)   NOT NULL,
-  Coach_ssn           INT UNIQUE    NOT NULL,
-  Assistant_Coach_Ssn INT UNIQUE,
-  University          VARCHAR(45)   NOT NULL,
-  Team_rank           INT UNIQUE    NOT NULL,
-  Win                 INT DEFAULT 0 NOT NULL,
-  Lost                INT DEFAULT 0 NOT NULL,
-  Tie                 INT DEFAULT 0 NOT NULL,
-  CONSTRAINT PK_Team_Id
-    PRIMARY KEY (Id)
-);
-
-# Create table PLAYERS if it does not already exist
-CREATE TABLE IF NOT EXISTS PLAYERS
-(
-  Ssn          INT UNIQUE    NOT NULL,
-  Team_id      INT UNIQUE    NOT NULL,
-  Name         VARCHAR(45)   NOT NULL,
-  Address      VARCHAR(60)   NOT NULL,
-  Birth_date   DATE          NOT NULL,
-  Position     VARCHAR(45)   NOT NULL,
-  University   VARCHAR(45)   NOT NULL,
-  Years_team   INT DEFAULT 0 NOT NULL,
-  Class        VARCHAR(45)   NOT NULL,
-  Players_rank INT DEFAULT 0 NOT NULL,
-  Time_allstar INT DEFAULT 0 NOT NULL,
-  CONSTRAINT CHK_Players_Rank
-    CHECK (Players_rank BETWEEN 0 AND 10),
-  CONSTRAINT PK_Players_Ssn
-    PRIMARY KEY (Ssn),
-  CONSTRAINT FK_Players_Team_Id
-    FOREIGN KEY (Team_id)
-      REFERENCES CHAMPIONSHIP_TEAM (Id)
-      ON DELETE RESTRICT
-      ON UPDATE CASCADE
-);
-
 # Create table COACH if it does not already exist
 CREATE TABLE IF NOT EXISTS COACH
 (
-  Ssn         INT UNIQUE    NOT NULL,
+  SSN         INT UNIQUE    NOT NULL,
   Name        VARCHAR(45)   NOT NULL,
   Address     VARCHAR(60),
   Birth_date  DATE          NOT NULL,
@@ -59,63 +18,100 @@ CREATE TABLE IF NOT EXISTS COACH
   Years_total INT DEFAULT 0 NOT NULL,
   Champs_wins INT DEFAULT 0 NOT NULL,
   Semifinals  INT DEFAULT 0 NOT NULL,
-  CONSTRAINT PK_Coach_Ssn
-    PRIMARY KEY (Ssn)
+  CONSTRAINT PK_COACH_SSN
+    PRIMARY KEY (SSN)
 );
 
 # Create table ASSISTANT_COACH if it does not already exist
 CREATE TABLE IF NOT EXISTS ASSISTANT_COACH
 (
-  Ssn            INT UNIQUE    NOT NULL,
+  SSN            INT UNIQUE    NOT NULL,
   Name           VARCHAR(45)   NOT NULL,
   Address        VARCHAR(60),
   Birth_date     DATE          NOT NULL,
-  Team_id        INT UNIQUE    NOT NULL,
   University     VARCHAR(45)   NOT NULL,
   Years_uni      INT DEFAULT 0 NOT NULL,
   Years_total    INT DEFAULT 0 NOT NULL,
   Specialization VARCHAR(45)   NOT NULL,
-  CONSTRAINT PK_Ass_Coach_Ssn
-    PRIMARY KEY (Ssn)
+  CONSTRAINT PK_ASSISTANT_COACH_SSN
+    PRIMARY KEY (SSN)
 );
 
-# Add Coach_name as FK in table CHAMPIONSHIP_TEAM referencing COACH table
-ALTER TABLE CHAMPIONSHIP_TEAM
-  ADD CONSTRAINT FK_Team_Coach_Ssn
-    FOREIGN KEY (Coach_ssn)
-      REFERENCES COACH (Ssn)
+# Create table CHAMPIONSHIP_TEAM if it does not already exist
+CREATE TABLE IF NOT EXISTS CHAMPIONSHIP_TEAM
+(
+  ID                  INT UNIQUE    NOT NULL,
+  Team_name           VARCHAR(45)   NOT NULL,
+  COACH_SSN           INT UNIQUE    NOT NULL,
+  ASSISTANT_COACH_SSN INT UNIQUE,
+  University          VARCHAR(45)   NOT NULL,
+  Team_rank           INT UNIQUE    NOT NULL,
+  Win                 INT DEFAULT 0 NOT NULL,
+  Lost                INT DEFAULT 0 NOT NULL,
+  Tie                 INT DEFAULT 0 NOT NULL,
+  CONSTRAINT PK_TEAM_ID
+    PRIMARY KEY (ID),
+  CONSTRAINT FK_TEAM_COACH_SSN
+    FOREIGN KEY (COACH_SSN)
+      REFERENCES COACH (SSN)
       ON DELETE RESTRICT
       ON UPDATE CASCADE,
-  Add CONSTRAINT FK_Team_Ass_Coach_Ssn
-    FOREIGN KEY (Assistant_Coach_Ssn)
-      REFERENCES ASSISTANT_COACH (Ssn)
+  CONSTRAINT FK_TEAM_ASSISTANT_COACH_SSN
+    FOREIGN KEY (ASSISTANT_COACH_SSN)
+      REFERENCES ASSISTANT_COACH (SSN)
       ON DELETE RESTRICT
-      ON UPDATE CASCADE;
+      ON UPDATE CASCADE
+);
+
+# Create table PLAYERS if it does not already exist
+CREATE TABLE IF NOT EXISTS PLAYERS
+(
+  SSN                   INT UNIQUE    NOT NULL,
+  CHAMPIONSHIP_TEAMS_ID INT UNIQUE    NOT NULL,
+  Name                  VARCHAR(45)   NOT NULL,
+  Address               VARCHAR(60)   NOT NULL,
+  Birth_date            DATE          NOT NULL,
+  Position              VARCHAR(45)   NOT NULL,
+  University            VARCHAR(45)   NOT NULL,
+  Years_team            INT DEFAULT 0 NOT NULL,
+  Class                 VARCHAR(45)   NOT NULL,
+  Time_allstar          INT DEFAULT 0 NOT NULL,
+  Player_rank           INT DEFAULT 0 NOT NULL,
+  CONSTRAINT CHK_Player_Rank
+    CHECK (Player_rank BETWEEN 0 AND 10),
+  CONSTRAINT PK_PLAYERS_SSN
+    PRIMARY KEY (SSN),
+  CONSTRAINT FK_PLAYERS_TEAM_ID
+    FOREIGN KEY (CHAMPIONSHIP_TEAMS_ID)
+      REFERENCES CHAMPIONSHIP_TEAM (ID)
+      ON DELETE RESTRICT
+      ON UPDATE CASCADE
+);
 
 # Create table ALLSTAR_GAME if it does not already exist
 CREATE TABLE IF NOT EXISTS ALLSTAR_GAME
 (
-  Date                DATE          NOT NULL,
-  Location            VARCHAR(60)   NOT NULL,
-  Team_id             INT UNIQUE    NOT NULL,
-  Coach_ssn           INT UNIQUE    NOT NULL,
-  Assistant_coach_ssn INT UNIQUE    NOT NULL,
-  Score               INT DEFAULT 0 NOT NULL,
-  CONSTRAINT PK_Game_Date_Location
+  Date                  DATE                        NOT NULL,
+  Location              VARCHAR(60)                 NOT NULL,
+  CHAMPIONSHIP_TEAMS_ID INT UNIQUE                  NOT NULL,
+  COACH_SSN             INT UNIQUE                  NOT NULL,
+  ASSISTANT_COACH_SSN   INT UNIQUE,
+  Score                 VARCHAR(10) DEFAULT '0 - 0' NOT NULL,
+  CONSTRAINT PK_GAME_Date_Location
     PRIMARY KEY (Date, Location),
-  CONSTRAINT FK_Game_Players_Team_Id
-    FOREIGN KEY (Team_id)
-      REFERENCES CHAMPIONSHIP_TEAM (Id)
+  CONSTRAINT FK_GAME_TEAM_ID
+    FOREIGN KEY (CHAMPIONSHIP_TEAMS_ID)
+      REFERENCES CHAMPIONSHIP_TEAM (ID)
       ON DELETE RESTRICT
       ON UPDATE CASCADE,
-  CONSTRAINT FK_Game_Coach_Ssn
-    FOREIGN KEY (Coach_ssn)
-      REFERENCES COACH (Ssn)
+  CONSTRAINT FK_GAME_COACH_SSN
+    FOREIGN KEY (COACH_SSN)
+      REFERENCES COACH (SSN)
       ON DELETE RESTRICT
       ON UPDATE CASCADE,
-  CONSTRAINT FK_Game_Ass_Coach_Ssn
-    FOREIGN KEY (Assistant_coach_ssn)
-      REFERENCES ASSISTANT_COACH (Ssn)
+  CONSTRAINT FK_GAME_ASSISTANT_COACH_SSN
+    FOREIGN KEY (ASSISTANT_COACH_SSN)
+      REFERENCES ASSISTANT_COACH (SSN)
       ON DELETE RESTRICT
       ON UPDATE CASCADE
 );
@@ -123,18 +119,18 @@ CREATE TABLE IF NOT EXISTS ALLSTAR_GAME
 # Create table ALLSTAR_NOMINEES if it does not already exist
 CREATE TABLE IF NOT EXISTS ALLSTAR_NOMINEES
 (
-  Ssn      INT UNIQUE  NOT NULL,
-  Date     DATE        NOT NULL,
-  Location VARCHAR(60) NOT NULL,
-  CONSTRAINT PK_Nominees_Ssn_Date_Location
-    PRIMARY KEY (Ssn, Date, Location),
-  CONSTRAINT FK_Nominees_Date_Location
-    FOREIGN KEY (Date, Location)
+  PLAYERS_SSN           INT UNIQUE  NOT NULL,
+  ALLSTAR_GAME_Date     DATE        NOT NULL,
+  ALLSTAR_GAME_Location VARCHAR(60) NOT NULL,
+  CONSTRAINT PK_NOMINEES_SSN_Date_Location
+    PRIMARY KEY (PLAYERS_SSN, ALLSTAR_GAME_Date, ALLSTAR_GAME_Location),
+  CONSTRAINT FK_NOMINEES_GAME_Date_Location
+    FOREIGN KEY (ALLSTAR_GAME_Date, ALLSTAR_GAME_Location)
       REFERENCES ALLSTAR_GAME (Date, Location)
       ON DELETE RESTRICT
       ON UPDATE CASCADE,
-  CONSTRAINT FK_Nominees_Players_Ssn
-    FOREIGN KEY (Ssn)
+  CONSTRAINT FK_NOMINEES_PLAYERS_SSN
+    FOREIGN KEY (PLAYERS_SSN)
       REFERENCES PLAYERS (Ssn)
       ON DELETE RESTRICT
       ON UPDATE CASCADE
